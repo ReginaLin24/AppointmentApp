ce627ce3a7330433f5c517da21312120fbb9a885
ReginaLin24 <reginaalin99@gmail.com>
2023-05-30 10:14:11 +1000

    initial push

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,34 @@
 var express = require("express");
 const path = require("path");
 var router = express.Router();
+const LaunchDarkly = require("launchdarkly-node-server-sdk");
+const {log} = require("console");
+require("dotenv").config();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/", async (req, res, next) => {
   // res.render('index', { title: 'Express' });
+  // res.sendFile(path.join(__dirname, "/views/index.html"));
+
+  // try {
+  const client = LaunchDarkly.init("sdk-7a4f697c-6712-4a70-8716-6d9e3ca2719f");
+  console.log(process.env.LAUNCHDARKLY_SDK_KEY);
+  await client.waitForInitialization();
+  // } catch (error) {
+  //   const errorString = JSON.stringify(error);
+  //   res.send(errorString);
+  //   return;
+  // }
+
+  let showFeature = await client.variation(
+    "newDataField",
+    {key: "anonymous"},
+    false
+  );
+  if (showFeature) {
+    res.sendFile(path.join(__dirname, "/views/newIndex.html"));
+    return;
+  }
   res.sendFile(path.join(__dirname, "/views/index.html"));
 });
 
